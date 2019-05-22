450e20aa7bb6724247426680d08bbf5c1dccbb5a
Juan Jovel <juanjosejovel1999@outlook.com>
2019-05-22 14:00:27 -0400

    Futher GUI development

--- src/capstone/scorebook/gui/AppController.java
+++ src/capstone/scorebook/gui/AppController.java
@@ -3,13 +3,24 @@ package capstone.scorebook.gui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 
 public class AppController {
 		@FXML
-		private Button registerButton;
+		private Button registerButton, exitButton;
+		@FXML
+		private AnchorPane athleteRegisterPane, mainMenuPane;
 		
 		
+		public void exit (ActionEvent e) {
+			System.exit(0);
+		}
+		
+		public void registerAthletes(ActionEvent e) {
+			// Change scene
+			
+		}
 
 		
 		
