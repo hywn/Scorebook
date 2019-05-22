fd48a452d6f982ebd46d8f3234cb3dcb95a78756
Juan Jovel <juanjosejovel1999@outlook.com>
2019-05-22 11:43:05 -0400

    add gui

---
+++ src/capstone/scorebook/gui/AppController.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class AppController {
+		@FXML
+		private Button registerButton;
+		
+		
+
+		
+		
+		
+	}
