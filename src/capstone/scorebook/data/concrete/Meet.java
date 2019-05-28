b9e7e829b8bbe29485a718883ef07481e8ec7ca9
james <seawee65@live.com>
2019-05-28 18:44:15 -0400

    misc fixes (#44)
    - improved autocomplete
    - clicking on green removes confirmation message
    - added rounds to meets
    - added practices

--- src/capstone/scorebook/data/concrete/Meet.java
+++ src/capstone/scorebook/data/concrete/Meet.java
@@ -9,6 +9,7 @@ public class Meet extends Location {
 
 	private static final String // remember that meet ID itself is handled by IDStorable and address ID is handled by Location
 		COLUMN_DATE = "date",
+		COLUMN_ROUNDS = "rounds",
 		COLUMN_SEASON = "season",
 		COLUMN_INDOOR = "indoor", // note: this is an integer; 0 is false, 1 is true
 		COLUMN_FAHRENHEIT_AVG = "tempAvg";
@@ -18,10 +19,11 @@ public class Meet extends Location {
 
 	private Meet(ResultSet rs) { super(STRUCT, rs); }
 
-	public Meet(String addressID, String date, String season, int isIndoor, int tempFahrenheitAvg) {
+	public Meet(String addressID, String date, int rounds, String season, int isIndoor, int tempFahrenheitAvg) {
 
 		super(STRUCT, addressID,
 		      COLUMN_DATE, date,
+		      COLUMN_ROUNDS, rounds,
 		      COLUMN_SEASON, season,
 		      COLUMN_INDOOR, isIndoor,
 		      COLUMN_FAHRENHEIT_AVG, tempFahrenheitAvg);
@@ -31,16 +33,18 @@ public class Meet extends Location {
 	@Override
 	protected String generateID() {
 
-		return getAddressID() + getDate() + getTempAvgFahrenheit();
+		return getRounds() + getIsIndoor() + getAddressID() + getDate() + getTempAvgFahrenheit();
 
 	}
 
 	public String getDate() { return getValue(COLUMN_DATE); }
+	public int getRounds() { return getValue(COLUMN_ROUNDS); }
 	public String getSeason() { return getValue(COLUMN_SEASON); }
 	public int getIsIndoor() { return getValue(COLUMN_INDOOR); }
 	public int getTempAvgFahrenheit() { return getValue(COLUMN_FAHRENHEIT_AVG); }
 
 	public String toString() {
-		return String.format("%s at %s", getDate(), getAddressID());
+		return String.format("%s at %s, %s", getDate(), getAddressID(),
+				     (getRounds() == 0) ? "practice" : getRounds() + " rounds");
 	}
 }
---
+++ src/capstone/scorebook/data/concrete/ScoreThrow.java
@@ -0,0 +1,30 @@
+package capstone.scorebook.data.concrete;
+
+import capstone.scorebook.data.StorableStruct;
+import capstone.scorebook.data.datatype.Score;
+
+import java.sql.ResultSet;
+
+public abstract class ScoreThrow extends Score { // TODO: one day it would be nice to have consistent naming
+
+	private final static String
+		COLUMN_ROUND = "round",
+		COLUMN_ORDER = "ord",
+		COLUMN_DISTANCE = "distance";
+
+	public ScoreThrow(StorableStruct struct, ResultSet rs) { super(struct, rs); }
+
+	public ScoreThrow(StorableStruct struct, String meetID, String athleteID, String weatherID, int round, int order, int distance) {
+
+		super(struct, meetID, athleteID, weatherID,
+		      COLUMN_ROUND, round,
+		      COLUMN_ORDER, order,
+		      COLUMN_DISTANCE, distance);
+
+	}
+
+	public int getOrder() { return getValue(COLUMN_ORDER); }
+	public int getRound() { return getValue(COLUMN_ROUND); }
+	public int getDistance() { return getValue(COLUMN_DISTANCE); }
+
+}
