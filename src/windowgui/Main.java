406deb354ba825f5a52e2f8a29098679ea9faafd
Juan Jovel <juanjosejovel1999@outlook.com>
2019-05-28 11:32:35 -0400

    This is an update
    This update fixes everything that Developer Huang ruined in the application while I was taking a sick day.

--- src/windowgui/Main.java
+++ src/windowgui/Main.java
@@ -1,14 +1,15 @@
 package windowgui;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
 
 public class Main extends Application {
 	private double xOffset = 0, yOffset = 0;
@@ -16,15 +17,29 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getClassLoader().getResource("windowgui/fxml/MainMenu.fxml"));
+			AnchorPane root = (AnchorPane) FXMLLoader
+					.load(getClass().getClassLoader().getResource("windowgui/fxml/MainMenu.fxml"));
 			Scene scene = new Scene(root, 600, 600);
 			scene.setFill(Color.TRANSPARENT);
 			root.setBackground(Background.EMPTY);
+			root.setOnMousePressed(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent e) {
+					xOffset = e.getSceneX();
+					yOffset = e.getSceneY();
+				}
+			});
+			root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent e) {
+					primaryStage.setX(e.getScreenX() - xOffset);
+					primaryStage.setY(e.getScreenY() - yOffset);
+				}
+			});
 			primaryStage.initStyle(StageStyle.TRANSPARENT);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
